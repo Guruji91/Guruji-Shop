1c51ff285250c21409b9e5a932a7f23814af4aa0
Guruji <78545781+Guruji91@users.noreply.github.com>
2023-05-21 22:36:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,14 +7,19 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js"
 import cors from "cors";
-import path, { dirname } from "path"
-
+import path from "path"
+import { fileURLToPath } from "url";
 
 // configure env
 dotenv.config();
 
 // database config
 connectDB();
+
+// es module fix
+const __fileName = fileURLToPath(import.meta.url)
+
+const __dirname = path.dirname(__fileName)
 
 // rest object
 const app = express();
@@ -23,7 +28,7 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(morgan("dev"));
-app.use(express.static(path.join(--dirname, './client/build')))
+app.use(express.static(path.join(__dirname, './client/build')))
 
 // routes
 app.use("/api/v1/auth", authRoutes);
@@ -32,7 +37,7 @@ app.use("/api/v1/product", productRoutes)
 
 // rest api
 app.get("*", function(req, res) {
-  res.sendFile(path.join(--dirname,  './client/build/index.html'));
+  res.sendFile(path.join(__dirname,  './client/build/index.html'));
 });
 
 //  PORT
